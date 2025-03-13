4604f513fbecbf435f215e40285178eb82e07405
Gitch-Hiker <samohp1126@gmail.com>
2025-03-13 18:29:38 +0900

    feat#28-InsertDataRequestDto 정의. Controller, Service 스켈레톤 추가

---
+++ indicator/src/main/java/com/smashup/indicator/module/gamerhint/controller/dto/GamerHintController.java
@@ -0,0 +1,53 @@
+package com.smashup.indicator.module.gamerhint.controller.dto;
+
+import com.smashup.indicator.common.util.AbstractRestController;
+import com.smashup.indicator.module.gamerhint.controller.dto.request.InsertDataListRequestDto;
+import com.smashup.indicator.module.gamerhint.controller.dto.request.InsertDataRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/hints")
+@RequiredArgsConstructor
+
+@Slf4j
+public class GamerHintController extends AbstractRestController {
+
+//    private final MemberService memberService;
+
+
+    // 테스트 => API 테스트 필요
+    @PutMapping("/data")
+    public ResponseEntity<Map<String, Object>> insertData(
+            @RequestBody InsertDataListRequestDto dto
+    ) throws Exception {
+        try {
+//            log.debug("insertData: {}", dto);
+            System.out.println(dto.toString());
+//            memberService.updateFolderChange(dto);
+            return handleSuccess(dto);
+        } catch (Exception e) {
+            return handleError(e.getMessage());
+        }
+    }
+//
+//    // 기능 : 모든 folder에 있는 프로젝트 조회 => API 테스트 성공
+//    @GetMapping("/folders/{UserId}")
+//    public ResponseEntity<Map<String, Object>> getProjectInFolder(
+//            @PathVariable Long UserId
+//    ) throws Exception {
+//        try {
+//            log.debug("getProjectInFolder: {}", UserId);
+//            List<ProjectMemberFolderResponseProjection> result = memberService.getProjectInFolder(UserId);
+//            return handleSuccess(result);
+//        } catch (Exception e) {
+//            return handleError(e.getMessage());
+//        }
+//    }
+
+}
