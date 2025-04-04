ed44d09b1d780fb29e166898b87b04f5a196e696
Gitch-Hiker <131760773+Gitch-Hiker@users.noreply.github.com>
2025-04-04 14:00:12 +0900

    feat#87 - GamerHintMatrixSubService 수정.

--- indicator/src/main/java/com/smashup/indicator/module/gamerhint/service/impl/GamerHintMatrixSubService.java
+++ indicator/src/main/java/com/smashup/indicator/module/gamerhint/service/impl/GamerHintMatrixSubService.java
@@ -23,7 +23,7 @@ public class GamerHintMatrixSubService {
     private final MongoTemplate mongoTemplate;
 
 
-    // 현재 batchCount의 document 2개 가져오기.
+    // 현재 batchCount의 document class개수*2개  가져오기.
     @Transactional
     public List<MatrixDocument> getDocumentByBatch(String inputPatchVersion, Integer batchCount) throws Exception {
         Query query = new Query();
@@ -69,42 +69,42 @@ public class GamerHintMatrixSubService {
         return result;
     }
 
-    // 마지막 배치 (가장 최신 배치) load
-    @Transactional
-    public List<MatrixDocument> getLatestDocument(String inputPatchVersion) throws Exception {
-        // 1. 가장 마지막 batchCount 하나만 가져옴 => batchCount 안전하게 가져오기
-        String regex1 = "^"+inputPatchVersion;
-        Query batchCountCheckQuery = new Query();
-        batchCountCheckQuery.addCriteria(Criteria.where("_id").regex(regex1));
-        batchCountCheckQuery.with(Sort.by(Sort.Direction.DESC, "_id"));
-        batchCountCheckQuery.limit(1);
-
-        MatrixDocument lastBatchDoc = mongoTemplate.findOne(batchCountCheckQuery, MatrixDocument.class);
-
-        // 검색결과가 없음 => inputPatchVersion 에 해당되는 Document 가 없음.
-        if (lastBatchDoc == null) {
-
-            return null; // 예외처리 필요 => 호출 위치상 inputPatchVersion 에 해당되는 Document가 있을때만 호출됨.
-        }
-
-        // 2. 마지막 batchCount를 추출
-        String lastBatchCount = lastBatchDoc.getId().split("/")[1];
-
-        // 3. 마지막 batchCount에 해당하는 C, T를 가져옴
-        Query finalQuery = new Query();
-        String regex2 = String.join("/", regex1,lastBatchCount,"");
-
-        finalQuery.addCriteria(Criteria.where("_id").regex(regex2));
-
-        List<MatrixDocument> results = mongoTemplate.find(finalQuery, MatrixDocument.class);
-
-        // 만약 C, T 중 하나만 존재하는 경우에 대한 처리
-        if (results.size() == 1) {
-            // 로그 추가 or 예외 처리 (C, T가 항상 있어야 하는데 1개만 존재할 경우 대비)
-            // System.out.println("경고: " + lastBatchCount + "의 C 또는 T 중 하나가 누락됨");
-        }
-        return results;
-    }
+//    // 마지막 배치 (가장 최신 배치) load
+//    @Transactional
+//    public List<MatrixDocument> getLatestDocument(String inputPatchVersion) throws Exception {
+//        // 1. 가장 마지막 batchCount 하나만 가져옴 => batchCount 안전하게 가져오기
+//        String regex1 = "^"+inputPatchVersion;
+//        Query batchCountCheckQuery = new Query();
+//        batchCountCheckQuery.addCriteria(Criteria.where("_id").regex(regex1));
+//        batchCountCheckQuery.with(Sort.by(Sort.Direction.DESC, "_id"));
+//        batchCountCheckQuery.limit(1);
+//
+//        MatrixDocument lastBatchDoc = mongoTemplate.findOne(batchCountCheckQuery, MatrixDocument.class);
+//
+//        // 검색결과가 없음 => inputPatchVersion 에 해당되는 Document 가 없음.
+//        if (lastBatchDoc == null) {
+//
+//            return null; // 예외처리 필요 => 호출 위치상 inputPatchVersion 에 해당되는 Document가 있을때만 호출됨.
+//        }
+//
+//        // 2. 마지막 batchCount를 추출
+//        String lastBatchCount = lastBatchDoc.getId().split("/")[1];
+//
+//        // 3. 마지막 batchCount에 해당하는 C, T를 가져옴
+//        Query finalQuery = new Query();
+//        String regex2 = String.join("/", regex1,lastBatchCount,"");
+//
+//        finalQuery.addCriteria(Criteria.where("_id").regex(regex2));
+//
+//        List<MatrixDocument> results = mongoTemplate.find(finalQuery, MatrixDocument.class);
+//
+//        // 만약 C, T 중 하나만 존재하는 경우에 대한 처리
+//        if (results.size() == 1) {
+//            // 로그 추가 or 예외 처리 (C, T가 항상 있어야 하는데 1개만 존재할 경우 대비)
+//            // System.out.println("경고: " + lastBatchCount + "의 C 또는 T 중 하나가 누락됨");
+//        }
+//        return results;
+//    }
 
     @Transactional
     public List<MatrixDocument> generateDocument(String inputPatchVersion, Integer batchCount) throws Exception {
@@ -114,26 +114,30 @@ public class GamerHintMatrixSubService {
         List<Integer> classPool = poolManager.getClassPool();
         List<Integer> mapPool = poolManager.getMapPool();
         List<Integer> playerNumPool = poolManager.getPlayerNumPool();
-        List<String> idList = generateMatrixId(classPool,mapPool,playerNumPool);
-        for (int i = 0; i < types.length; i++) {
-            // doc 하나 만들기 => id랑 type만 다르게 하면 됨.
-            MatrixDocument doc = MatrixDocument.builder()
-                    .id(String.join("/", inputPatchVersion,batchCount+"",types[i]))
-                    .type(types[i])
-                    .cardPool(poolManager.getCardPool())
-                    .matrixMap(new HashMap<>())
-                    .build();
+        List<String> idList = generateMatrixId(mapPool,playerNumPool);
+        // 클래스 별로 생성
+        for(Integer classCode : classPool){
+            for (int i = 0; i < types.length; i++) {
+                // doc 하나 만들기 => id랑 type만 다르게 하면 됨.
+                MatrixDocument doc = MatrixDocument.builder()
+                        .id(String.join("/", inputPatchVersion,batchCount+"",types[i],classCode+""))
+                        .type(types[i])
+                        .cardPool(poolManager.getClassCardPoolMap().get(classCode))
+                        .matrixMap(new HashMap<>())
+                        .build();
 
-            // 모든 ID + Matrix 스켈레톤을 matrixMap에 put.
-            for (String id : idList) {
-                doc.getMatrixMap().put(id, generateSkeletonMatrix());
+                // 모든 ID + Matrix 스켈레톤을 matrixMap에 put.
+                for (String id : idList) {
+                    doc.getMatrixMap().put(id, generateSkeletonMatrix(classCode));
+                }
+                // 리턴 값에 저장.
+                result.add(doc);
             }
-            // 리턴 값에 저장.
-            result.add(doc);
         }
 
         return result;
     }
+
     // win 버전
     @Transactional
     public List<WinMatrixDocument> generateWinDocument(String inputPatchVersion, Integer batchCount) throws Exception {
@@ -143,35 +147,39 @@ public class GamerHintMatrixSubService {
         List<Integer> classPool = poolManager.getClassPool();
         List<Integer> mapPool = poolManager.getMapPool();
         List<Integer> playerNumPool = poolManager.getPlayerNumPool();
-        List<String> idList = generateMatrixId(classPool,mapPool,playerNumPool);
-        for (int i = 0; i < types.length; i++) {
-            // doc 하나 만들기 => id랑 type만 다르게 하면 됨.
-            WinMatrixDocument doc = WinMatrixDocument.builder()
-                    .id(String.join("/", inputPatchVersion,batchCount+"",types[i]))
-                    .type(types[i])
-                    .cardPool(poolManager.getCardPool())
-                    .matrixMap(new HashMap<>())
-                    .build();
+        List<String> idList = generateMatrixId(mapPool,playerNumPool);
+        // 클래스 별로 생성
+        for(Integer classCode : classPool){
+            for (int i = 0; i < types.length; i++) {
+                // doc 하나 만들기 => id랑 type만 다르게 하면 됨.
+                WinMatrixDocument doc = WinMatrixDocument.builder()
+                        .id(String.join("/", inputPatchVersion,batchCount+"",types[i],classCode+""))
+                        .type(types[i])
+                        .cardPool(poolManager.getClassCardPoolMap().get(classCode))
+                        .matrixMap(new HashMap<>())
+                        .build();
 
-            // 모든 ID + Matrix 스켈레톤을 matrixMap에 put.
-            for (String id : idList) {
-                doc.getMatrixMap().put(id, generateSkeletonMatrix());
+                // 모든 ID + Matrix 스켈레톤을 matrixMap에 put.
+                for (String id : idList) {
+                    doc.getMatrixMap().put(id, generateSkeletonMatrix(classCode));
+                }
+                // 리턴 값에 저장.
+                result.add(doc);
             }
-            // 리턴 값에 저장.
-            result.add(doc);
         }
 
         return result;
     }
 
     // 재사용 할 Matrix 스켈레톤 생성. deepCopy가 없어서..
-    public List<List<Integer>> generateSkeletonMatrix() throws Exception {
+    public List<List<Integer>> generateSkeletonMatrix(int classCode) throws Exception {
         List<List<Integer>> skeletonMatrix = new ArrayList<>();
         List<Integer> row = new ArrayList<>();
-        for (int col = 0; col < poolManager.getCardPool().size(); col++) { // col 채우기
+        List<Integer> classCardPool = poolManager.getClassCardPoolMap().get(classCode);
+        for (int col = 0; col < classCardPool.size(); col++) { // col 채우기
             row.add(0);
         }
-        for (int i = 0; i < poolManager.getCardPool().size()+1; i++) { // row 채우기
+        for (int i = 0; i < classCardPool.size()+1; i++) { // row 채우기
             skeletonMatrix.add(new ArrayList<>(row));
         }
         return skeletonMatrix;
@@ -180,23 +188,16 @@ public class GamerHintMatrixSubService {
 
 
     // MatrixId 생성 로직 [완료]
-    public List<String> generateMatrixId(List<Integer> classPool, List<Integer> mapPool,List<Integer> playerNumPool) throws Exception {
+    public List<String> generateMatrixId(List<Integer> mapPool,List<Integer> playerNumPool) throws Exception {
         List<String> result = new ArrayList<>();
-//        // pool 세팅
-//        classPool.add(-1);
-//        mapPool.add(-1);
-//        playerNumPool.add(-1);
-        //
-        for(Integer classID : classPool){
-            for(Integer mapID : mapPool){
-                for(Integer playerNum : playerNumPool){
-                    String matrixId = String.join("/", classID+"",mapID+"",playerNum+"");
-                    result.add(matrixId);
-                }
+
+        for(Integer mapID : mapPool){
+            for(Integer playerNum : playerNumPool){
+                String matrixId = String.join("/", mapID+"",playerNum+"");
+                result.add(matrixId);
             }
         }
-//        // BONUS 추가
-//        result.add("-1/-1/-1");
+
         return result;
     }
 
@@ -216,7 +217,9 @@ public class GamerHintMatrixSubService {
         List<Integer> round2 = playerLog.getRound2Set();
         List<Integer> round3 = playerLog.getRound3Set();
 
-        Map<Integer, Integer> cardPoolIndexMap = poolManager.getCardPoolIndex();
+//        Map<Integer, Integer> cardPoolIndexMap = poolManager.getCardPoolIndex();
+//        Map<Integer, Integer> cardPoolIndexMap = poolManager.getCardPoolIndex();
+        Map<Integer, Integer> cardPoolIndexMap = poolManager.getClassCardPoolIndexMap().get(playerLog.getClassCode());
         for (int i = 0; i < round1.size(); i++) {
             Integer cardId = round1.get(i);
             Integer cardIndex = cardPoolIndexMap.get(cardId);
@@ -314,7 +317,8 @@ public class GamerHintMatrixSubService {
         merged.addAll(playerLog.getRound2Set());
         merged.addAll(playerLog.getRound3Set());
         // 카드 ID 를 인덱스로 전환
-        Map<Integer, Integer> cardPoolIndexMap = poolManager.getCardPoolIndex();
+//        Map<Integer, Integer> cardPoolIndexMap = poolManager.getCardPoolIndex();
+        Map<Integer, Integer> cardPoolIndexMap = poolManager.getClassCardPoolIndexMap().get(playerLog.getClassCode());
         for (int i = 0; i < merged.size(); i++) {
             Integer cardId = merged.get(i);
             Integer cardIndex = cardPoolIndexMap.get(cardId);
@@ -331,10 +335,10 @@ public class GamerHintMatrixSubService {
     }
 
     // private 써도 되겠지? 이 서비스에서만 쓸건데 [완료]
-    public void updateMatrix(MatrixDocument docC, List<String> targetMatrixIdList, List<int[]> coexistenceXYList) {
+    public void updateMatrix(MatrixDocument doc, List<String> targetMatrixIdList, List<int[]> xyList) {
         for (String id : targetMatrixIdList){
-            List<List<Integer>> matrix= docC.getMatrixMap().get(id);
-            for(int[] xy:coexistenceXYList){
+            List<List<Integer>> matrix= doc.getMatrixMap().get(id);
+            for(int[] xy:xyList){
                 int row = xy[0];
                 int col = xy[1];
                 int oldValue = matrix.get(row).get(col);
